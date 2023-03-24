16a83ca27485a7aa65a70a2bd8f1dd6386671738
Piergiusto Ranieri <pierranieri@hotmail.com>
2023-03-24 13:00:52 +0100

    Component Button and Function

---
+++ ChuckNorris---React/src/components/Button.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../styles/Button.css'
+const Buttons = ({text, variant, clbk, styles}) => {
+
+  function renderClasses(){
+    let classes = [
+      'buttons', variant, styles
+    ]
+    return classes.join(" ")
+  }
+  return (
+    <div>
+      <button className={renderClasses()} onClick={clbk}>{text}</button>
+    </div>
+  )
+}
+
+export default Buttons
